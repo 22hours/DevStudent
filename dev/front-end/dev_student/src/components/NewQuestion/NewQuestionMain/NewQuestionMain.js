789ad99906d0e283f3c2fbeea8ee356d2cb544b0
winterlood <king199777@gmail.com>
2020-04-08 19:02:33 +0900

    add markdown editor

--- dev/front-end/dev_student/src/components/NewQuestion/NewQuestionMain/NewQuestionMain.js
+++ dev/front-end/dev_student/src/components/NewQuestion/NewQuestionMain/NewQuestionMain.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Input } from "reactstrap";
-import { Button } from "@material-ui/core";
+import { Input, Button } from "reactstrap";
 import TagInput from "../../TagInput/TagInput";
 import "./NewQuestionMain.css";
+import MarkdownEditor from "../../MarkdownEditor/MarkdownEditor";
 const NewQuestionMain = ({ user, title, setTitle, body, setBody, tags, setTags, handleSubmit }) => {
     return (
         <div className="new-question-main-wrapper">
@@ -25,38 +25,38 @@ const NewQuestionMain = ({ user, title, setTitle, body, setBody, tags, setTags, 
                     <span className="new-question-content-header-span">Body</span> <br />
                     <span className="new-question-notice-span">궁금한점을 자세하고 구체적으로 적어주세요</span>
                 </div>
-                <Input
+                <MarkdownEditor comment={body} setComment={setBody} />
+                {/* <Input
+                    id="question-body-input"
                     value={body}
                     onChange={({ target: { value } }) => setBody(value)}
                     size="large"
+                    placeholder="ex> 도대체 이건 어떻게 하는 건가요?"
                     type="textarea"
-                ></Input>
+                ></Input> */}
             </div>
             <div className="new-question-main-element-box">
                 <div className="new-question-title-header-span-warpper">
-                    <br />
-                    <span className="new-question-notice-span">태그를 달아주세요! [현재 사용 불가]</span>
+                    <span className="new-question-content-header-span">Tags</span> <br />
+                    <span className="new-question-notice-span">태그를 달아주세요!</span>
                 </div>
-                {/* <Input
-                 onChange={({target : {value}}) => setTags(value)}
-                className="question-title" placeholder="Tags : Ex> Javascript..." /> */}
                 <TagInput tags={tags} setTags={setTags} />
             </div>
             <div className="new-question-main-element-box">
-                <div className="submit-left">
+                {/* <div className="submit-left">
                     <Button variant="contained" color="secondary">
                         Back
                     </Button>
-                </div>
+                </div> */}
                 <div className="submit-right">
                     <Button
                         onClick={() => {
                             handleSubmit(user, title, body, tags);
                         }}
                         variant="contained"
-                        color="primary"
+                        color="info"
                     >
-                        Submit
+                        SUBMIT
                     </Button>
                 </div>
             </div>
