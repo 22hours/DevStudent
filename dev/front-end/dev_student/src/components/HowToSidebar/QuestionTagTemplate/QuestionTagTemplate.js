7902a8591d00c6a8de9c084ec8afc4c8be4a00f3
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-04-08 21:54:26 +0900

    HowToSidebar fix
    1. HowtoSidebar 그림자 제거
    2. stack overflow ui 로 태그 변경

--- dev/front-end/dev_student/src/components/HowToSidebar/QuestionTagTemplate/QuestionTagTemplate.js
+++ dev/front-end/dev_student/src/components/HowToSidebar/QuestionTagTemplate/QuestionTagTemplate.js
@@ -1,24 +1,22 @@
-import React,{Component} from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import QuestionTagHeader from'../QuestionTagHeader/QuestionTagHeader';
-import './QuestionTagTemplate.css';
-import QuestionTagList from '../QuestionTagList/QuestionTagList';
+import React, { Component } from "react";
+import QuestionTagHeader from "../QuestionTagHeader/QuestionTagHeader";
+import "./QuestionTagTemplate.css";
+import QuestionTagList from "../QuestionTagList/QuestionTagList";
 
 class QuestionTagTemplate extends Component {
-    render(){
-        const {tag} = this.props;
-        
-        return(
-            <div className="question-tag-wrapper" >
-                <div  className="question-tag-header-wrapper">
-                    <QuestionTagHeader/>
-                </div>
-                <div className="question-tag-list-wrapper">
-                    <QuestionTagList tag={tag}>
-                    </QuestionTagList>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { tag } = this.props;
+
+    return (
+      <div className="question-tag-wrapper">
+        <div className="question-tag-header-wrapper">
+          <QuestionTagHeader />
+        </div>
+        <div className="question-tag-list-wrapper">
+          <QuestionTagList tag={tag}></QuestionTagList>
+        </div>
+      </div>
+    );
+  }
 }
 export default QuestionTagTemplate;
