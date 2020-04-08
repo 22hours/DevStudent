0a7d4dd819886e130a178ec69ba782f454b461a4
winterlood <king199777@gmail.com>
2020-04-08 19:47:39 +0900

    Fix NewQuestion Template's margin , apply sidebar

--- dev/front-end/dev_student/src/components/NewQuestion/NewQuestionTemplate/NewQuestionTemplate.js
+++ dev/front-end/dev_student/src/components/NewQuestion/NewQuestionTemplate/NewQuestionTemplate.js
@@ -1,40 +1,39 @@
 import React, { useState, useContext } from "react";
 import "./NewQuestionTemplate.css";
-import { Container, Row, Col, Input } from "reactstrap";
-import { Button } from "@material-ui/core";
+import { Container } from "reactstrap";
 import UserContext from "../../../Context/UserContext";
-import TagInput from "../../TagInput/TagInput";
 import NewQuestionMain from "../NewQuestionMain/NewQuestionMain";
-import NewQuestionSidebarTemplate from "../NewQuestionSideBar/NewQuestionSideBarTemplate/NewQuestionSideBarTemplate";
+import NewQuestionSideBarTemplate from "../NewQuestionSideBar/NewQuestionSideBarTemplate/NewQuestionSideBarTemplate";
 const NewQuestionTemplate = ({ handleSubmit }) => {
     const { user } = useContext(UserContext);
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [tags, setTags] = useState([]);
-
     return (
         <div className="new-question-wrapper">
-            <div className="new-question-header-row">
-                <span>{user}님 &nbsp;</span>
-                <span>무엇이든 물어보세요!</span>
-            </div>
-            <div className="new-question-body-warpper">
-                <div className="new-question-main-col">
-                    <NewQuestionMain
-                        user={user}
-                        title={title}
-                        setTitle={setTitle}
-                        body={body}
-                        setBody={setBody}
-                        tags={tags}
-                        setTags={setTags}
-                        handleSubmit={handleSubmit}
-                    />
+            <Container>
+                <div className="new-question-header-row">
+                    <span>{user}님 &nbsp;</span>
+                    <span>무엇이든 물어보세요!</span>
                 </div>
-                <div className="new-question-sidebar-col">
-                    <NewQuestionSidebarTemplate/>
+                <div className="new-question-body-warpper">
+                    <div className="new-question-main-col">
+                        <NewQuestionMain
+                            user={user}
+                            title={title}
+                            setTitle={setTitle}
+                            body={body}
+                            setBody={setBody}
+                            tags={tags}
+                            setTags={setTags}
+                            handleSubmit={handleSubmit}
+                        />
+                    </div>
+                    <div className="new-question-sidebar-col">
+                        <NewQuestionSideBarTemplate />
+                    </div>
                 </div>
-            </div>
+            </Container>
         </div>
     );
 };
