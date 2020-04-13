9f892b682dcdcf2772503da16bccb9b8e827559c
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-04-13 21:39:08 +0900

    Register Page 수정
    1. 닉네임 중복검사
    2. 비밀번호 규칙 적용
    3. 이메일 인증 확인

--- dev/front-end/dev_student/src/pages/Register.js
+++ dev/front-end/dev_student/src/pages/Register.js
@@ -1,52 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { Input } from "reactstrap";
 import RegisterPageTemplate from "page-template/RegisterPageTemplate/RegisterPageTemplate";
+import { CHECK_DUPLICATE_NICKNAME } from "mutation/mutations";
+import { useMutation } from "react-apollo";
 
 const Register = () => {
     const [userEmail, setUserEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [repwd, setRepwd] = useState("");
+    const [rePwd, setRePwd] = useState("");
     const [schoolName, setSchoolName] = useState("");
     const [nickName, setNickName] = useState("");
+    const [checkDuplicateNickname, { data }] = useMutation(CHECK_DUPLICATE_NICKNAME);
+    const [nickCheck, setNickCheck] = useState("false");
+    const [pwdCheck, setPwdCheck] = useState("false");
+    const [pwdRuleCheck, setPwdRuleCheck] = useState("false");
+
+    useLayoutEffect(() => {
+        if (data == null) return;
+        if (data.checkDuplicateNickname.count === 0) {
+            alert("중복된 닉네임 입니다.");
+            return;
+        } else {
+            alert("사용 가능한 닉네임 입니다.");
+            setNickCheck("true");
+        }
+    }, [data]);
 
     const RepwInputRenderer = () => {
-        if (password === repwd) {
+        if (password !== rePwd) {
             return (
                 <Input
-                    value={repwd}
-                    onChange={({ target: { value } }) => setRepwd(value)}
-                    valid
+                    invalid
+                    value={rePwd}
+                    onChange={({ target: { value } }) => setRePwd(value)}
                     type="password"
                     name="password"
-                    id="passwordCheck"
-                    placeholder="password check"
+                    id="inputPassword"
+                    placeholder="password"
+                    onBlur={RepwInputRenderer}
                 />
             );
         } else {
-            return (
-                <Input
-                    value={repwd}
-                    onChange={({ target: { value } }) => setRepwd(value)}
-                    invalid
-                    type="password"
-                    name="password"
-                    id="passwordCheck"
-                    placeholder="password check"
-                />
-            );
+            setPwdCheck("true");
+            return <div>pwd 확인</div>;
         }
     };
+
     return (
         <RegisterPageTemplate
             email={userEmail}
             setEmail={setUserEmail}
             password={password}
             setPassword={setPassword}
+            rePwd={rePwd}
+            setRePwd={setRePwd}
             nickName={nickName}
             setNickName={setNickName}
             schoolName={schoolName}
             setSchoolName={setSchoolName}
             RepwInputRenderer={RepwInputRenderer}
+            checkDuplicateNickname={checkDuplicateNickname}
+            nickCheck={nickCheck}
+            pwdCheck={pwdCheck}
+            pwdRuleCheck={pwdRuleCheck}
+            setPwdRuleCheck={setPwdRuleCheck}
+            setPwdCheck={setPwdCheck}
         ></RegisterPageTemplate>
     );
 };
