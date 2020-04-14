73162aacaedad0672d5ca2fcbcca7810fd9079b7
winterlood <king199777@gmail.com>
2020-04-14 23:34:49 +0900

    update jobscheduler

--- dev/front-end/dev_student/src/module/RecruitScheduleModule/RecruitScheduleModule.js
+++ dev/front-end/dev_student/src/module/RecruitScheduleModule/RecruitScheduleModule.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Container } from "reactstrap";
+import { Container, Collapse, Alert } from "reactstrap";
+import "./RecruitScheduleModule.css";
 const RecruitScheduleModule = () => {
     const [item, setItem] = useState();
+    const [remain, setRemain] = useState();
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const [isOpenText, setIsOpenText] = useState("더보기");
     useEffect(() => {
         axios
             .get("https://raw.githubusercontent.com/jojoldu/junior-recruit-scheduler/master/db.json")
             .then((response) => {
-                console.log(response.data.recruits);
                 setItem(
-                    response.data.recruits.map(({ endDate, link, description }) => (
-                        <div key={link}>
-                            <p>{description}</p>
-                            <p>{endDate}</p>
-                        </div>
+                    response.data.recruits.slice(0, 3).map(({ endDate, link, description }) => (
+                        <a href={link}>
+                            <div className="recruit-item" key={link}>
+                                <p id="description">{description}</p> <p id="endData"> 기간 : {endDate}</p>
+                            </div>
+                        </a>
+                    ))
+                );
+                setRemain(
+                    response.data.recruits.slice(4).map(({ endDate, link, description }) => (
+                        <a href={link}>
+                            <div className="recruit-item" key={link}>
+                                <p id="description">{description}</p> <p id="endData"> 기간 : {endDate}</p>
+                            </div>
+                        </a>
                     ))
                 );
             })
@@ -24,7 +38,35 @@ const RecruitScheduleModule = () => {
     const Recurit = async () => {};
     return (
         <React.Fragment>
-            <Container>{item}</Container>
+            <div className="RecruitScheduleModule">
+                <Container>
+                    <div className="recruit-header">
+                        <p id="header">실시간 채용 정보</p>
+                        <p id="reference">
+                            출처 :&nbsp;
+                            <a href="https://github.com/jojoldu/junior-recruit-scheduler">
+                                주니어 개발자를 위한 취업 정보
+                            </a>
+                        </p>
+                    </div>
+                    {item}
+                    <Collapse isOpen={isOpen}>{remain}</Collapse>
+                    <div
+                        onClick={() => {
+                            if (isOpenText === "더보기") {
+                                setIsOpenText("닫기");
+                            } else {
+                                setIsOpenText("더보기");
+                            }
+                            toggle();
+                        }}
+                        className="recruit-item"
+                        style={{ cursor: "pointer" }}
+                    >
+                        <p id="description">{isOpenText}</p>
+                    </div>
+                </Container>
+            </div>
         </React.Fragment>
     );
 };
