dbc4108f924bdcbede4d1a1d5f6635879c4c2bc4
Favian <damin8@naver.com>
2020-04-20 20:58:35 +0900

    update
    - 메일 log 출력 삭제
    
    - 메일 필요없는 text 삭제

--- dev/back-end/loginserver/src/main/java/loginserver/loginserver/Module/AuthMailSend.java
+++ dev/back-end/loginserver/src/main/java/loginserver/loginserver/Module/AuthMailSend.java
@@ -17,7 +17,6 @@ public class AuthMailSend {
     private RandMaker randMaker;
 
     public String authMailSend(String email, String nickname) {
-        System.out.println("authMailSend 시작");
         String key = randMaker.getKey(false, 20);
         System.out.println("authMailSend 키테스트");
         System.out.println(key);
@@ -27,17 +26,12 @@ public class AuthMailSend {
         String htmlStr = "<h2>안녕하세요. DevStudent 계정인증 메일입니다!</h2><br><br>"
                 + "<h3>" + nickname + "님</h3>" + "<p>인증 코드를 입력하시면 인증에 성공합니다 :) <p>인증 코드 : "
                 //+ "<a href='http://devstudent/emailCheck/"+key+"'>인증하기</a></p>";
-                + key + "'>인증하기</p>";
+                + key + "</p>";
         try {
-            System.out.println("setSubject");
             mail.setSubject("[본인인증] 안녕하세요. DevStudent 계정인증 메일입니다!", "utf-8");
-            System.out.println("setText");
             mail.setText(htmlStr, "utf-8", "html");
-            System.out.println("addRecipient");
             mail.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-            System.out.println("send");
             javaMailSender.send(mail);
-            System.out.println("mail 끝");
         } catch (MessagingException e) {
             e.printStackTrace();
             return "error";
