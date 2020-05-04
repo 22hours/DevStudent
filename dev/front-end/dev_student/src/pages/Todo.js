1c550ffbffd5ddc4ae41793190865522d1ed2d4e
winterlood <king199777@gmail.com>
2020-05-04 16:32:06 +0900

    :wrench:  Remove unuse components

--- dev/front-end/dev_student/src/pages/Todo.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import NotImplementedPageTemplate from "page-template/NotImplementedPageTemplate/NotImplementedPageTemplate";
-
-class Todo extends Component {
-    render() {
-        return <NotImplementedPageTemplate />;
-    }
-}
-export default Todo;
