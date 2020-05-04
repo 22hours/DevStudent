f1c9d56f8f4ceb80771c0e3d97473a4d818a37d3
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-05-04 20:07:40 +0900

    Update HotQuestionItem.js

--- dev/front-end/dev_student/src/item/HotQuestionItem/HotQuestionItem.js
+++ dev/front-end/dev_student/src/item/HotQuestionItem/HotQuestionItem.js
@@ -19,11 +19,16 @@ const HotQuestionItem = ({ id, views, title }) => {
         paddingBottom: "0.40rem",
         // color:"#034390"
     };
+    var hotTitle = "";
+    hotTitle = title;
+    if (hotTitle.length > 25) {
+        hotTitle = hotTitle.substr(0, 25) + "...";
+    }
     return (
         <div width="100%">
             <div className="hot-links-list-group-item">
                 <a href={"/howto/question/" + id}>
-                    <LinkIcon style={{ fontSize: "14px", color: "gray" }} /> {title}{" "}
+                    <LinkIcon style={{ fontSize: "14px", color: "gray" }} /> {hotTitle}{" "}
                 </a>
                 <span id="views">
                     {" "}
