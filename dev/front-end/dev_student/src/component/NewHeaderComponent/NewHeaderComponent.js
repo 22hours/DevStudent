ccd8b0e1165b2bbbc908c955acc3d23549e0de25
winterlood <king199777@gmail.com>
2020-05-04 16:31:23 +0900

    :art:  Remove legacy header & replace new header

--- dev/front-end/dev_student/src/component/NewHeaderComponent/NewHeaderComponent.js
+++ dev/front-end/dev_student/src/component/NewHeaderComponent/NewHeaderComponent.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./NewHeaderComponent.css";
-import { Container } from "reactstrap";
+import { Container, Collapse } from "reactstrap";
 import { Link } from "react-router-dom";
 
 // icon
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import MenuIcon from "@material-ui/icons/Menu";
+
+//imgs
+import mobile_logo from "img/header/mobile-logo.png";
+import mobile_text_logo from "img/header/mobile-text-logo.png";
 
 const MemberArea = ({ nickname, handleNicknameClick }) => {
     return (
@@ -27,10 +32,16 @@ const GuestArea = () => {
 
 const NewHeaderComponent = ({ nickname, location }) => {
     const [clicked, setClicked] = useState("home");
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggle = () => setIsOpen(!isOpen);
 
     useEffect(() => {
         // 현재 위치에 불 켜기
         setClicked(location.pathname.split("/")[1]);
+
+        // collapse 무조건 끄기
+        setIsOpen(false);
     }, [location.pathname]);
 
     const handleNicknameClick = () => {
@@ -41,6 +52,11 @@ const NewHeaderComponent = ({ nickname, location }) => {
         // if (nickname) {
         // } else {
         // }
+        // return (
+        //     <div className="user-outter">
+        //         <GuestArea nickname={"winterlood"} handleNicknameClick={handleNicknameClick} />
+        //     </div>
+        // );
         return (
             <div className="user-outter">
                 <MemberArea nickname={"winterlood"} handleNicknameClick={handleNicknameClick} />
@@ -88,11 +104,45 @@ const NewHeaderComponent = ({ nickname, location }) => {
                     <UserArea />
                 </div>
             </Container>
-            <Container className="header-wrapper mobile-only">
-                <div className="header-logo-col">1</div>
-                {/* <div className="header-menu-col">2</div> */}
-                <div className="header-button-col">3</div>
-            </Container>
+            <div className="header-wrapper-mobile mobile-only">
+                <div className="header-menu-col" onClick={() => toggle()}>
+                    <MenuIcon />
+                </div>
+
+                <div className="header-logo-col">
+                    <div className="img-box">
+                        <img id="mobile-logo" src={mobile_text_logo}></img>
+                    </div>
+                </div>
+
+                <div className="header-user-col">
+                    <UserArea />
+                </div>
+            </div>
+            <Collapse isOpen={isOpen}>
+                <div className="collapse-wrapper">
+                    <div className={"collapse-item-outter " + (clicked === "" ? "mobile-on" : "mobile-off")}>
+                        <Link to="/">
+                            <div className="collapse-item-inner">Home</div>
+                        </Link>
+                    </div>
+                    <div className={"collapse-item-outter " + (clicked === "howto" ? "mobile-on" : "mobile-off")}>
+                        <Link to="/howto">
+                            <div className="collapse-item-inner">HowTo</div>
+                        </Link>
+                    </div>
+                    <div className={"collapse-item-outter " + (clicked === "rule" ? "mobile-on" : "mobile-off")}>
+                        <Link to="/rule">
+                            <div className="collapse-item-inner">Rule</div>
+                        </Link>
+                    </div>
+                    <div className={"collapse-item-outter " + (clicked === "devnote" ? "mobile-on" : "mobile-off")}>
+                        <Link to="/devnote">
+                            <div className="collapse-item-inner">DevNote</div>
+                        </Link>
+                    </div>
+                </div>
+            </Collapse>
         </div>
     );
 };
---
+++ dev/front-end/dev_student/src/pages/Todo.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import NotImplementedPageTemplate from "page-template/NotImplementedPageTemplate/NotImplementedPageTemplate";
+
+class Todo extends Component {
+    render() {
+        return <NotImplementedPageTemplate />;
+    }
+}
+export default Todo;
