ea9f4bbd13877006873c3d65467d7b800e9b286c
Favian <damin8@naver.com>
2020-05-07 14:59:17 +0900

    update
    - cors 처리
    
    - 해당 도메인만 접근 가능

--- dev/back-end/imageServer/src/main/java/imageserver/image/Configuration/WebSecurityConfig.java
+++ dev/back-end/imageServer/src/main/java/imageserver/image/Configuration/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package imageserver.image.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -12,7 +18,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
+                .cors()
+                .and()
                 .authorizeRequests()
                 .anyRequest().permitAll();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://devstudent.web.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
