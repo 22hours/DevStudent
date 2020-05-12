908c70e285123f381bfd22517b7deeb89f18a5a2
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-05-12 14:25:46 +0900

    Update

--- dev/front-end/dev_student/src/component/NewHeaderComponent/NewHeaderComponent.js
+++ dev/front-end/dev_student/src/component/NewHeaderComponent/NewHeaderComponent.js
@@ -11,6 +11,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 //imgs
 import mobile_logo from "img/header/mobile-logo.png";
 import mobile_text_logo from "img/header/mobile-text-logo.png";
+import devstu_text_logo_empty from "img/footer/devstu_text_logo_empty.png";
 
 const MemberArea = () => {
     return <MypageDropdownButtonModule />;
@@ -29,8 +30,17 @@ const GuestArea = () => {
 const NewHeaderComponent = ({ nickname, location }) => {
     const [clicked, setClicked] = useState("home");
     const [isOpen, setIsOpen] = useState(true);
-
+    const [scroll, setScroll] = useState(0);
     const toggle = () => setIsOpen(!isOpen);
+    const handleScroll = (event) => {
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        console.log(scrollPosition);
+        setScroll(scrollPosition);
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [1]);
 
     useEffect(() => {
         // 현재 위치에 불 켜기
@@ -52,48 +62,57 @@ const NewHeaderComponent = ({ nickname, location }) => {
         );
     };
     return (
-        <div className="NewHeaderComponent">
-            <Container className="header-wrapper pc-only">
-                <Link to="/">
-                    <div className="header-logo-col">
-                        <div className="img-box">
-                            <img id="brand-img" src="/img/devstu_text_logo_empty.png"></img>
+        <div className={"NewHeaderComponent"}>
+            <div className={"header-wrapper pc-only " + (clicked === "" && scroll === 0 ? "zero" : "none")}>
+                <Container>
+                    <Link to="/">
+                        <div className="header-logo-col">
+                            <div className="img-box">
+                                <img
+                                    id="brand-img"
+                                    src={
+                                        clicked === "" && scroll === 0
+                                            ? "/img/devstu_text_logo_empty_white.png"
+                                            : "/img/devstu_text_logo_empty.png"
+                                    }
+                                ></img>
+                            </div>
+                        </div>
+                    </Link>
+                    <div className="header-menu-col">
+                        <div className="menu-outter">
+                            <div className={"menu-box " + (clicked === "" ? "on" : "off")}>
+                                <Link to="/" className="menu-item">
+                                    Home
+                                </Link>
+                            </div>
+                            <div className={"menu-box " + (clicked === "howto" ? "on" : "off")}>
+                                <Link to="/howto" className="menu-item">
+                                    HowTo
+                                </Link>
+                            </div>
+                            <div className={"menu-box " + (clicked === "rule" ? "on" : "off")}>
+                                <Link to="/rule" className="menu-item">
+                                    Rule
+                                </Link>
+                            </div>
+                            <div className={"menu-box " + (clicked === "devnote" ? "on" : "off")}>
+                                <Link to="/devnote" className="menu-item">
+                                    DevNote
+                                </Link>
+                            </div>
+                            <div className={"menu-box " + (clicked === "issue" ? "on" : "off")}>
+                                <Link to="/issue" className="menu-item">
+                                    Issues
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                </Link>
-                <div className="header-menu-col">
-                    <div className="menu-outter">
-                        <div className={"menu-box " + (clicked === "" ? "on" : "off")}>
-                            <Link to="/" className="menu-item">
-                                Home
-                            </Link>
-                        </div>
-                        <div className={"menu-box " + (clicked === "howto" ? "on" : "off")}>
-                            <Link to="/howto" className="menu-item">
-                                HowTo
-                            </Link>
-                        </div>
-                        <div className={"menu-box " + (clicked === "rule" ? "on" : "off")}>
-                            <Link to="/rule" className="menu-item">
-                                Rule
-                            </Link>
-                        </div>
-                        <div className={"menu-box " + (clicked === "devnote" ? "on" : "off")}>
-                            <Link to="/devnote" className="menu-item">
-                                DevNote
-                            </Link>
-                        </div>
-                        <div className={"menu-box " + (clicked === "issue" ? "on" : "off")}>
-                            <Link to="/issue" className="menu-item">
-                                Issues
-                            </Link>
-                        </div>
+                    <div className="header-user-col">
+                        <UserArea />
                     </div>
-                </div>
-                <div className="header-user-col">
-                    <UserArea />
-                </div>
-            </Container>
+                </Container>
+            </div>
             <div className="header-wrapper-mobile mobile-only">
                 <div className="header-menu-col" onClick={() => toggle()}>
                     <MenuIcon />
@@ -109,35 +128,37 @@ const NewHeaderComponent = ({ nickname, location }) => {
                     <UserArea />
                 </div>
             </div>
-            <Collapse isOpen={isOpen}>
-                <div className="collapse-wrapper">
-                    <div className={"collapse-item-outter " + (clicked === "" ? "mobile-on" : "mobile-off")}>
-                        <Link to="/">
-                            <div className="collapse-item-inner">Home</div>
-                        </Link>
+            <div className="mobile-menu-collapse">
+                <Collapse isOpen={isOpen}>
+                    <div className="collapse-wrapper">
+                        <div className={"collapse-item-outter " + (clicked === "" ? "mobile-on" : "mobile-off")}>
+                            <Link to="/">
+                                <div className="collapse-item-inner">Home</div>
+                            </Link>
+                        </div>
+                        <div className={"collapse-item-outter " + (clicked === "howto" ? "mobile-on" : "mobile-off")}>
+                            <Link to="/howto">
+                                <div className="collapse-item-inner">HowTo</div>
+                            </Link>
+                        </div>
+                        <div className={"collapse-item-outter " + (clicked === "rule" ? "mobile-on" : "mobile-off")}>
+                            <Link to="/rule">
+                                <div className="collapse-item-inner">Rule</div>
+                            </Link>
+                        </div>
+                        <div className={"collapse-item-outter " + (clicked === "devnote" ? "mobile-on" : "mobile-off")}>
+                            <Link to="/devnote">
+                                <div className="collapse-item-inner">DevNote</div>
+                            </Link>
+                        </div>
+                        <div className={"collapse-item-outter " + (clicked === "issue" ? "mobile-on" : "mobile-off")}>
+                            <Link to="/issue">
+                                <div className="collapse-item-inner">Issues</div>
+                            </Link>
+                        </div>
                     </div>
-                    <div className={"collapse-item-outter " + (clicked === "howto" ? "mobile-on" : "mobile-off")}>
-                        <Link to="/howto">
-                            <div className="collapse-item-inner">HowTo</div>
-                        </Link>
-                    </div>
-                    <div className={"collapse-item-outter " + (clicked === "rule" ? "mobile-on" : "mobile-off")}>
-                        <Link to="/rule">
-                            <div className="collapse-item-inner">Rule</div>
-                        </Link>
-                    </div>
-                    <div className={"collapse-item-outter " + (clicked === "devnote" ? "mobile-on" : "mobile-off")}>
-                        <Link to="/devnote">
-                            <div className="collapse-item-inner">DevNote</div>
-                        </Link>
-                    </div>
-                    <div className={"collapse-item-outter " + (clicked === "issue" ? "mobile-on" : "mobile-off")}>
-                        <Link to="/issue">
-                            <div className="collapse-item-inner">Issues</div>
-                        </Link>
-                    </div>
-                </div>
-            </Collapse>
+                </Collapse>
+            </div>
         </div>
     );
 };
