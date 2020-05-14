509e8c78de1490861d0bcf3abeb99442c3a7b68d
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-05-14 21:09:40 +0900

    :wrench: Update Searchbar

--- dev/front-end/dev_student/src/module-template/HowToListModuleTemplate/HowToListModuleTemplate.js
+++ dev/front-end/dev_student/src/module-template/HowToListModuleTemplate/HowToListModuleTemplate.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Container } from "reactstrap";
 import { useQuery } from "@apollo/react-hooks";
-import { FIND_QUESTIONS_BY_TAG, findAllQuestionsPage } from "query/queries";
+import { FIND_QUESTIONS_BY_TAG, findAllQuestionsPage, FIND_QUESTIONS_BY_OPTION } from "query/queries";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import ServerError from "pages/ServerError";
 
@@ -25,7 +25,7 @@ const TagProvider = ({ param, nowTag, pageNum, setQuestionCount }) => {
                 <CircularProgress />
             </div>
         );
-    if (error) return <ServerError />;
+    if (error) return <p>Error!</p>;
 
     setQuestionCount(Object.keys(data.findQuestionsByTags).length);
 
@@ -61,13 +61,13 @@ const NonTagProvider = ({ param, nowTag, pageNum, setQuestionCount, questionAll 
                 <CircularProgress />
             </div>
         );
-    if (error) return <ServerError />;
+    if (error) return <p>Error!</p>;
 
     setQuestionCount(questionAll);
 
     if (data === null) {
         alert("Error!");
-        if (error) return <ServerError />;
+        if (error) return <p>Error!</p>;
     }
     return data.findAllQuestions.map(
         ({ _id, title, author, tags, date, content, answerCount, views, previews, adoptedAnswerId, likesCount }) => (
@@ -91,34 +91,84 @@ const NonTagProvider = ({ param, nowTag, pageNum, setQuestionCount, questionAll 
     );
 };
 
-const DataProvider = ({ param, nowTag, pageNum, setQuestionCount, questionCount, questionAll }) => {
-    if (nowTag) {
+const SearchProvider = ({ param, nowSearch, pageNum, setQuestionCount }) => {
+    nowSearch = decodeURI(nowSearch);
+    const { loading, error, data } = useQuery(FIND_QUESTIONS_BY_OPTION, {
+        variables: { param: "date", requiredCount: 10, pageNum: pageNum, option: "content", searchContent: nowSearch },
+    });
+    if (loading)
         return (
-            <TagProvider
+            <div>
+                <CircularProgress />
+            </div>
+        );
+    if (error) return <p>Error!</p>;
+
+    setQuestionCount(Object.keys(data.findQuestionsByOption).length);
+
+    return data.findQuestionsByOption.map(
+        ({ _id, title, author, tags, date, content, answerCount, views, previews, adoptedAnswerId, likesCount }) => (
+            <HowToBoxItem
+                id={_id}
+                key={_id}
+                authorNickname={author.nickname}
+                authorPoint={author.point}
+                authorGrade={author.grade}
+                title={title}
+                answers={answerCount}
+                views={views}
+                date={date}
+                dateToText={timeForToday(date)}
+                previews={previews}
+                tags={tags}
+                likesCount={likesCount}
+                adoptedAnswerId={adoptedAnswerId}
+            ></HowToBoxItem>
+        )
+    );
+};
+
+const DataProvider = ({ param, nowTag, pageNum, setQuestionCount, questionCount, questionAll, nowSearch }) => {
+    if (nowSearch) {
+        return (
+            <SearchProvider
                 param={param}
-                nowTag={nowTag}
+                nowSearch={nowSearch}
                 pageNum={pageNum}
                 setQuestionCount={setQuestionCount}
                 questionCount={questionCount}
             />
         );
     } else {
-        return (
-            <NonTagProvider
-                param={param}
-                nowTag={nowTag}
-                pageNum={pageNum}
-                setQuestionCount={setQuestionCount}
-                questionCount={questionCount}
-                questionAll={questionAll}
-            />
-        );
+        if (nowTag) {
+            return (
+                <TagProvider
+                    param={param}
+                    nowTag={nowTag}
+                    pageNum={pageNum}
+                    setQuestionCount={setQuestionCount}
+                    questionCount={questionCount}
+                />
+            );
+        } else {
+            return (
+                <NonTagProvider
+                    param={param}
+                    nowTag={nowTag}
+                    pageNum={pageNum}
+                    setQuestionCount={setQuestionCount}
+                    questionCount={questionCount}
+                    questionAll={questionAll}
+                />
+            );
+        }
     }
 };
 
 const HowToListTemplate = ({ tags, location, questionAll }) => {
     const [param, setParam] = useState("date");
-    const [nowTag, setNowTag] = useState(location.search.split("=")[1]);
+    const [nowTag, setNowTag] = useState(location.search.split("tags=")[1]);
+    const [nowSearch, setSearch] = useState(location.search.split("search=")[1]);
     const [pageNum, setPage] = useState(1);
     const [questionCount, setQuestionCount] = useState(questionAll);
 
@@ -138,6 +188,7 @@ const HowToListTemplate = ({ tags, location, questionAll }) => {
                 <DataProvider
                     param={param}
                     nowTag={nowTag}
+                    nowSearch={nowSearch}
                     pageNum={pageNum}
                     questionCount={questionCount}
                     setQuestionCount={setQuestionCount}
