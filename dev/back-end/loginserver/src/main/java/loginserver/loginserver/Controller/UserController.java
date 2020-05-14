938b3c4da0584d287c3d7d500102cc076c606d2c
Favian <damin8@naver.com>
2020-05-15 02:40:33 +0900

    Update UserController.java
    - 쓰레드 관리
    
    - 비동기 처리

--- dev/back-end/loginserver/src/main/java/loginserver/loginserver/Controller/UserController.java
+++ dev/back-end/loginserver/src/main/java/loginserver/loginserver/Controller/UserController.java
@@ -1,5 +1,7 @@
 package loginserver.loginserver.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import loginserver.loginserver.Entity.Count;
 import loginserver.loginserver.Entity.User;
 import loginserver.loginserver.Entity.UserInfo;
@@ -10,11 +12,13 @@ import loginserver.loginserver.Module.Update.UpdateUserAuthState;
 import loginserver.loginserver.Repository.UserInfoRepository;
 import loginserver.loginserver.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.annotation.WebServlet;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/user/*")
@@ -36,30 +40,47 @@ public class UserController {
 
     @Async(value = "cuser")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public User createUser(@RequestBody User user) {
+    public CompletableFuture<String> createUser(@RequestBody User user) {
         String email = user.getEmail();
         String password = user.getPassword();
         String schoolName = user.getSchoolName();
-        return createUser.createUser(email, password, schoolName);
+        User tempUser = createUser.createUser(email, password, schoolName);
+        Gson gson = new Gson();
+        String json = gson.toJson(tempUser);
+        System.out.println("Request = " + user.toString());
+        System.out.println("Response = " + json);
+        return CompletableFuture.completedFuture(json);
     }
+
     @Async(value = "UAuthState")
     @RequestMapping(value = "/update/AuthState", method = RequestMethod.POST)
-    public User updateUserAuthState(@RequestBody User user) {
+    public CompletableFuture<String> updateUserAuthState(@RequestBody User user) {
         String authState = user.getAuthState();
-        return updateUserAuthState.updateUserAuthState(authState);
+        User tempUser = updateUserAuthState.updateUserAuthState(authState);
+        Gson gson = new Gson();
+        String json = gson.toJson(tempUser);
+        System.out.println("Request = " + user.toString());
+        System.out.println("Response = " + json);
+        return CompletableFuture.completedFuture(json);
     }
+
     @Async(value = "logindevstu")
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public User loginToServer(@RequestBody Map<String, String> map) {
+    public CompletableFuture<String> loginToServer(@RequestBody Map<String, String> map) {
         String email = map.get("email");
         String password = map.get("password");
         String ip = map.get("ip");
-        return login.login(email, password, ip);
+        User tempUser = login.login(email, password, ip);
+        Gson gson = new Gson();
+        String json = gson.toJson(tempUser);
+        System.out.println("Request = " + map.toString());
+        System.out.println("Response = " + json);
+        return CompletableFuture.completedFuture(json);
     }
     @Async(value = "nickname")
     @RequestMapping(value = "/create/nickname",method = RequestMethod.POST)
     //public User createNickname(@RequestBody User user) {// 중복은 0, 중복이 아닌 것은 1
-    public User createNickname(@RequestBody Map<String, String> map) {// 중복은 0, 중복이 아닌 것은 1
+    public CompletableFuture<String> createNickname(@RequestBody Map<String, String> map) {// 중복은 0, 중복이 아닌 것은 1
         String email = map.get("email");//user.getEmail();
         String nickname = map.get("nickname");//user.getNickname();
         String ip = map.get("ip");
@@ -67,23 +88,25 @@ public class UserController {
         {
             User tempUser = userRepository.findByEmail(email);
             if(!(tempUser.getAuthState().equals("Certificated")))
-                return new User();
+                return null;
             tempUser.setAccessToken(createNewAccessToken.create(nickname, email, ip));
             tempUser.setNickname(nickname);
             userRepository.save(tempUser);
             UserInfo userInfo = new UserInfo(email,nickname,tempUser.getSchoolName());
             userInfoRepository.save(userInfo);
-            return tempUser;
+            Gson gson = new Gson();
+            String json = gson.toJson(tempUser);
+            return CompletableFuture.completedFuture(json);
         }
-        return new User();
+        return null;
     }
     @Async(value = "duplicateEmail")
     @RequestMapping(value = "/check/email",method = RequestMethod.POST)
-    public Count checkDuplicateEmail(@RequestBody User user){
+    public CompletableFuture<String> checkDuplicateEmail(@RequestBody User user){
         String email = user.getEmail();
         if(!userRepository.existsByEmail(email)){
-            return new Count("true");
+            return CompletableFuture.completedFuture("true");
         }
-        return new Count("duplicated");
+        return CompletableFuture.completedFuture("duplicated");
     }
 }
