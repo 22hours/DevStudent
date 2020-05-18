d11e8d636bb720ebb69d86e909fd5e92bcff9138
Favian <damin8@naver.com>
2020-05-18 17:39:19 +0900

    imageServer Update
    - imageServer S3로 전환 완료
    
    - Test 완료
    
    todo
    
    EC2에 war파일 올려서, nginx랑 호환

--- dev/back-end/imageServer/src/main/java/imageserver/image/Service/S3Service.java
+++ dev/back-end/imageServer/src/main/java/imageserver/image/Service/S3Service.java
@@ -6,18 +6,27 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import imageserver.image.Entity.FileUploadData;
+import imageserver.image.Exception.FileUploadException;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Iterator;
 
 @Component
 public class S3Service {
     private AmazonS3 s3Client;
+
+    private final String temp = "linmemhnebggaijoinhajeoyndohsgauhenkwin/";
+    private final String real = "linjiewhngguyeamjhghoaonknmnioeebdnihgjloenewason/";
 
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
@@ -31,8 +40,8 @@ public class S3Service {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Setter
-    private String location;
+    @Autowired
+    private RandMaker randMaker;
 
     @PostConstruct
     public void setS3Client() {
@@ -44,11 +53,72 @@ public class S3Service {
                 .build();
     }
 
-    public String upload(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
+    public void deleteFiles() {
+        try {
+            System.out.println("Dummy Files Delete");
+            ObjectListing object_listing = s3Client.listObjects(bucket);
+            while (true) {
+                for (Iterator<?> iterator =
+                     object_listing.getObjectSummaries().iterator();
+                     iterator.hasNext(); ) {
+                    S3ObjectSummary summary = (S3ObjectSummary) iterator.next();
+                    String str = summary.getKey().substring(0, 4);
+                    if (str.equals("temp"))
+                        s3Client.deleteObject(bucket, summary.getKey());
+                }
 
-        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
-        return s3Client.getUrl(bucket, fileName).toString();
+                // more object_listing to retrieve?
+                if (object_listing.isTruncated()) {
+                    object_listing = s3Client.listNextBatchOfObjects(object_listing);
+                } else {
+                    break;
+                }
+            }
+            System.out.println("Delete Complete");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Delete Fail!!!");
+        }
+    }
+
+    public void upload(String date, String[] fileNames) {
+        try {
+            for (String fileName : fileNames) {
+                System.out.println("FileName = " + fileName);
+                String from = this.temp + fileName;
+                String to = real + date + "/" + fileName;
+                s3Client.copyObject(bucket, from, bucket, to);
+                s3Client.deleteObject(bucket, from);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("uploadRealFile Fail!!!");
+        }
+    }
+
+    public FileUploadData upload(MultipartFile file) throws IOException {
+        try {
+            String fileName = file.getOriginalFilename();
+            String tempFileName = null;
+            String rand = randMaker.getKey(false, 20);
+
+            if (fileName.contains(".."))
+                throw new FileUploadException("파일명에 부적합 문자가 포함되어 있습니다. " + fileName);
+
+            String str = fileName.substring(fileName.lastIndexOf('.'));
+            rand = rand + str;
+            fileName = temp + rand;
+
+            s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            FileUploadData fileUploadData = new FileUploadData(s3Client.getUrl(bucket, fileName).toString(), rand);
+
+            return fileUploadData;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("uploadDummyFile Fail!!!");
+            return null;
+        }
     }
 }
