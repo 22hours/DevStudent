12deb3cad2389fe1842c820cd6639003b84c9ede
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-05-19 20:16:30 +0900

    :wrench: Update Mypage to rest

--- dev/front-end/dev_student/src/pages/MyPage.js
+++ dev/front-end/dev_student/src/pages/MyPage.js
@@ -1,63 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-apollo";
 import MypageTemplate from "page-template/MypageTemplate/MypageTemplate";
-import { FIND_ALL_ALARMS, FIND_QUESTIONS_BY_OPTION } from "query/queries";
+//import { FIND_ALL_ALARMS, FIND_QUESTIONS_BY_OPTION } from "query/queries";
 import { Link } from "react-router-dom";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import ServerError from "pages/ServerError";
 
+import { POST, FIND_ALL_ALARMS, FIND_QUESTIONS_BY_OPTIONS } from "rest";
+
 const AlarmPovider = () => {
     const localData = JSON.parse(localStorage.getItem("user"));
     const user = localData?.nickname;
-    const { loading, error, data } = useQuery(FIND_ALL_ALARMS, {
-        variables: { nickname: user, pageNum: 1, requiredCount: 10 },
-    });
-    if (loading)
-        return (
-            <div>
-                <CircularProgress />
-            </div>
-        );
-    if (error) return <ServerError />;
-    return data.findAllAlarms.map(({ _id, question_id, content, date, respondent }) => (
-        <Link to={"/howto/question/" + question_id}>
-            <div key={question_id} className="item-box">
-                <span id="item-label">{date}</span>
-                <p id="item-value">
-                    {respondent} {content}
-                </p>
-            </div>
-        </Link>
-    ));
+
+    const [alarmData, setAlarmData] = useState();
+
+    const getAlarmData = async () => {
+        const data = await POST("post", FIND_ALL_ALARMS, {
+            nickname: user,
+            pageNum: 1,
+            requiredCount: 10,
+        });
+        setAlarmData(data);
+    };
+    useEffect(() => {
+        getAlarmData();
+    }, [1]);
+
+    if (alarmData) {
+        return alarmData.map(({ _id, question_id, content, date, respondent }) => (
+            <Link to={"/howto/question/" + question_id}>
+                <div key={question_id} className="item-box">
+                    <span id="item-label">{date}</span>
+                    <p id="item-value">
+                        {respondent} {content}
+                    </p>
+                </div>
+            </Link>
+        ));
+    } else {
+        return <p>loading</p>;
+    }
 };
 const MyContentProvider = () => {
     const localData = JSON.parse(localStorage.getItem("user"));
     const user = localData?.nickname;
-    const { loading, error, data } = useQuery(FIND_QUESTIONS_BY_OPTION, {
-        variables: { param: "date", option: "author", searchContent: user, pageNum: 1, requiredCount: 10 },
-    });
-    if (loading)
-        return (
-            <div>
-                <CircularProgress disableShrink size={24} />
-            </div>
-        );
-    if (error) return <ServerError />;
-    return data.findQuestionsByOption.map(({ _id, title, content, date, answerCount, likesCount, views }) => (
-        <Link to={"/howto/question/" + _id}>
-            <div key={_id} className="item-box">
-                <span id="item-label">{date}</span>
-                <p id="item-value">
-                    {title} <span>{views}</span>
-                </p>
-            </div>
-        </Link>
-    ));
+    const [questionData, setQuestionData] = useState();
+    const getQuestionData = async () => {
+        const data = await POST("post", FIND_QUESTIONS_BY_OPTIONS, {
+            param: "date",
+            option: "author",
+            searchContent: user,
+            pageNum: 1,
+            requiredCount: 10,
+        });
+        setQuestionData(data);
+    };
+    useEffect(() => {
+        getQuestionData();
+    }, [1]);
+
+    if (questionData) {
+        return questionData.map(({ _id, title, content, date, answerCount, likesCount, views }) => (
+            <Link to={"/howto/question/" + _id}>
+                <div key={_id} className="item-box">
+                    <span id="item-label">{date}</span>
+                    <p id="item-value">
+                        {title} <span>{views}</span>
+                    </p>
+                </div>
+            </Link>
+        ));
+    } else {
+        return <p>loading</p>;
+    }
 };
+
 const MyPage = () => {
-    const alramData = <AlarmPovider />;
+    const userAlarm = <AlarmPovider />;
     const myContent = <MyContentProvider />;
     const localData = JSON.parse(localStorage.getItem("user"));
-    return <MypageTemplate localData={localData} alarmData={alramData} myContent={myContent} />;
+
+    return <MypageTemplate localData={localData} userAlarm={userAlarm} myContent={myContent} />;
 };
 export default MyPage;
