34261f529346d2452a043cbd26cc2a9b1e2dde8b
winterlood <king199777@gmail.com>
2020-05-20 17:08:23 +0900

    :wrench: Migration alarm.js to rest

--- dev/front-end/dev_student/src/pages/Alarm.js
+++ dev/front-end/dev_student/src/pages/Alarm.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AlarmTemplate from "page-template/AlarmTemplate/AlarmTemplate";
-import { FIND_ALL_ALARMS } from "../query/queries";
-import { useQuery } from "react-apollo";
 import ServerError from "pages/ServerError";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
+// modules
+import NowLoading from "module/NowLoading/NowLoading";
+
+// rest
+import { POST, FIND_ALL_ALARMS } from "rest";
+
 const Alarm = () => {
     const nickname = JSON.parse(localStorage.getItem("user"))?.nickname;
-    const { loading, error, data } = useQuery(FIND_ALL_ALARMS, {
-        variables: { nickname: nickname, pageNum: 1, requiredCount: 10 },
-    });
-    if (loading)
-        return (
-            <div>
-                <CircularProgress />
-            </div>
-        );
-    if (error) return <ServerError />;
-    return <AlarmTemplate alarms={data.findAllAlarms} />;
+    const [alarmData, setAlarmData] = useState();
+    const getAlarmData = async () => {
+        const data = await POST("post", FIND_ALL_ALARMS, {
+            nickname: nickname,
+            pageNum: 1,
+            requiredCount: 10,
+        });
+        if (data) {
+            setAlarmData(data);
+        }
+    };
+    useEffect(() => {
+        getAlarmData();
+    }, [1]);
+
+    if (alarmData) {
+        return <AlarmTemplate alarms={alarmData} />;
+    } else {
+        return <NowLoading />;
+    }
 };
 
 export default Alarm;
