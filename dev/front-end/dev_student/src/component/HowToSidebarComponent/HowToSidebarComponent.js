23492926440da639991598d3a0dafc70a9475f0e
김효빈 <52522662+sg05138@users.noreply.github.com>
2020-05-22 20:26:52 +0900

    :wrench: :art: Add JobInfoTemplate in HowtoSidebar

--- dev/front-end/dev_student/src/component/HowToSidebarComponent/HowToSidebarComponent.js
+++ dev/front-end/dev_student/src/component/HowToSidebarComponent/HowToSidebarComponent.js
@@ -17,6 +17,7 @@ import HotQuestionItem from "item/HotQuestionItem/HotQuestionItem";
 
 // Queries
 import { POST, FIND_ALL_QUESTIONS } from "rest";
+import JobInfoModuleTemplate from "../../module-template/JobInfoModuleTemplate/JobInfoModuleTemplate";
 
 const HowToSidebarComponent = ({ tag, handleNewQuestion }) => {
     const btn_style = {
@@ -59,6 +60,9 @@ const HowToSidebarComponent = ({ tag, handleNewQuestion }) => {
                 </div>
                 <div>
                     <HotLinksModuleTemplate hotquestionList={hotquestionList}></HotLinksModuleTemplate>
+                </div>
+                <div>
+                    <JobInfoModuleTemplate />
                 </div>
             </div>
             <div style={{ marginBottom: "3.5%" }}></div>
---
+++ dev/front-end/dev_student/src/module-template/JobInfoModuleTemplate/JobInfoModuleTemplate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./JobInfoModuleTemplate.css";
+
+const JobInfoModuleTemplate = () => {
+    return (
+        <div className="sidebar-item-wrapper">
+            <div className="howto-job-info-header-wrapper">
+                <div className="item-header">실시간 채용정보 &nbsp;</div>
+            </div>
+            <div className="item-list-wrapper">
+                <div>안녕</div>
+            </div>
+        </div>
+    );
+};
+export default JobInfoModuleTemplate;
