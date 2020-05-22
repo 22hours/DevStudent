3e1a8c475ed1ef539b58b47a63e9469e57c527e5
winterlood <king199777@gmail.com>
2020-05-22 16:09:27 +0900

    :wrech: Update howto.js & home howto guide

--- dev/front-end/dev_student/src/module/HowToRuleModule/HowToRuleModule.js
+++ dev/front-end/dev_student/src/module/HowToRuleModule/HowToRuleModule.js
@@ -3,6 +3,7 @@ import "./HowToRuleModule.css";
 import { Container } from "reactstrap";
 
 // icons
+import LabelIcon from "@material-ui/icons/Label";
 import HelpIcon from "@material-ui/icons/Help";
 import TextsmsIcon from "@material-ui/icons/Textsms";
 import ThumbsUpDownIcon from "@material-ui/icons/ThumbsUpDown";
@@ -17,96 +18,55 @@ import { Link } from "react-router-dom";
 
 const HowToRuleModule = () => {
     const [clicked, setClicked] = useState(1);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            let now = clicked;
-            let next = (now + 1) % 7;
-            if (next === 0) next += 1;
-            setClicked(next);
-        }, 5000);
-        return () => clearInterval(interval);
-    }, [clicked]);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         let now = clicked;
+    //         let next = (now + 1) % 7;
+    //         if (next === 0) next += 1;
+    //         setClicked(next);
+    //     }, 5000);
+    //     return () => clearInterval(interval);
+    // }, [clicked]);
+    const helpmsg = "질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지";
     return (
         <div className="HowToRuleModule">
-            <Container>
+            <Container style={{ height: "auto", overflow: "hidden" }}>
                 <div className="howto-rule-header">
-                    DEVSTU 에서는 이렇게 질문과 답변을 주고받아요 누구나 할 수 있어요
+                    <span className="header">DEVSTU HOWTO 사용법</span>
+                    <div className="description">DEVSTU는 당신을 원한다!</div>
                 </div>
-                <div className="pc-only">
-                    <div className="howto-rule-item-col">
-                        <HowToRuleItem
-                            id={1}
-                            setClicked={setClicked}
-                            key={1}
-                            nowClicked={clicked}
-                            icon={<TextsmsIcon />}
-                            message={"편하게 질문 하기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-
-                        <HowToRuleItem
-                            nowClicked={clicked}
-                            setClicked={setClicked}
-                            id={2}
-                            key={2}
-                            icon={<ThumbsUpDownIcon />}
-                            message={"해답에 투표하기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-                        <HowToRuleItem
-                            nowClicked={clicked}
-                            setClicked={setClicked}
-                            id={3}
-                            key={3}
-                            icon={<QuestionAnswerIcon />}
-                            message={"질문에 답변하기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-                    </div>
-                    <div className="howto-rule-content-col">
-                        <HowToRuleContentItem nowClicked={clicked} />
-                    </div>
-                    <div className="howto-rule-item-col">
-                        <HowToRuleItem
-                            nowClicked={clicked}
-                            setClicked={setClicked}
-                            id={4}
-                            key={4}
-                            icon={<PeopleIcon />}
-                            message={"다함께 성장하기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-                        <HowToRuleItem
-                            nowClicked={clicked}
-                            setClicked={setClicked}
-                            id={5}
-                            key={5}
-                            icon={<CheckCircleIcon />}
-                            message={"답변을 채택하기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-                        <HowToRuleItem
-                            nowClicked={clicked}
-                            setClicked={setClicked}
-                            id={6}
-                            key={6}
-                            icon={<LoyaltyIcon />}
-                            message={"해시태그 달기"}
-                        >
-                            <HelpIcon />
-                        </HowToRuleItem>
-                    </div>
+                <div className="btn-col">
+                    <HowToRuleItem
+                        nowClicked={clicked}
+                        key={1}
+                        id={1}
+                        title={"질문하세요"}
+                        message={helpmsg}
+                        setClicked={setClicked}
+                        icon={<HelpIcon style={{ fontSize: "18px", marginBottom: "5px" }} />}
+                    />
+                    <HowToRuleItem
+                        nowClicked={clicked}
+                        key={2}
+                        id={2}
+                        title={"답변하세요"}
+                        message={
+                            "질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지"
+                        }
+                        setClicked={setClicked}
+                        icon={<HelpIcon style={{ fontSize: "18px", marginBottom: "5px" }} />}
+                    />
+                    <HowToRuleItem
+                        nowClicked={clicked}
+                        key={3}
+                        id={3}
+                        title={"질문하는법"}
+                        message={"질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지질문메세지"}
+                        setClicked={setClicked}
+                        icon={<HelpIcon style={{ fontSize: "18px", marginBottom: "5px" }} />}
+                    />
                 </div>
-                <div className="mobile-only">
-                    <div className="howto-rule-content-col">
-                        <HowToRuleContentItem nowClicked={clicked} />
-                    </div>
-                </div>
+                <div className="img-col pc-only">d</div>
             </Container>
         </div>
     );
